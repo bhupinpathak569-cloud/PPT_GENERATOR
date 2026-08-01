53945e8cb107bf7a0739fc6fe0801f091113de31
bhupinpathak569-cloud <bhupinpathak569@gmail.com>
2026-08-01 10:42:34 +0530

    Configure Streamlit layout and refine user input check
    Set Streamlit page layout to wide and updated user input condition.

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from tavily import TavilyClient
 import numpy as np
 import streamlit as st
 
+st.set_page_config(layout = "wide")
+
 st.title("Agentic PPT Generator")
 st.header("""User can generate, PPT, Images, and fetch latest news""")
 
@@ -123,7 +125,7 @@ tab1,tab2,tab3 = st.tabs(["Generate Image",
 
 user_input = st.text_area("Write prompt and cick enter")
 
-if (user_input) & (leader_agent):
+if (user_input):
   with tab1:
     if st.button("Click to Generate Image",key = "Image-Button"):
       with st.spinner("Running Agent"):
